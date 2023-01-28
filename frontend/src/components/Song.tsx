83d626d8f0af9c082b4111395dcb718eac9b0e68
Daniel <daniel.mekuriaw16@gmail.com>
2023-01-28 13:51:19 -0500

    Play/pause toggle

--- frontend/src/components/Song.tsx
+++ frontend/src/components/Song.tsx
@@ -1,6 +1,24 @@
-import { Card, Flex, CardBody, Box, Center, Spacer, Text, Image } from '@chakra-ui/react';
+import { Card, Flex, CardBody, Box, Center, Spacer, Text, Image} from '@chakra-ui/react';
+import {useState} from "react"
 
 function Song() {
+
+    const [play, setPlay] = useState(false); // Default state is false
+    const [buttonSource, setButtonSource] = useState('')
+
+    function handleClick () {
+
+        if (!play){ // Playing audio
+            setButtonSource("/stop-button.png")
+
+        } else { // Stopping audio
+            setButtonSource("/play-button.png")
+        }
+
+        setPlay(!play)
+        console.log("Clicked!")
+    }
+
     return (
         <Center>
             <Card width="50%">
@@ -13,7 +31,7 @@ function Song() {
                         </Box>
                         <Spacer />
                         <Center>
-                            <Image boxSize='70px' src="/play-button.png" />
+                            <Image onClick={handleClick} boxSize='70px' src={buttonSource} />
                         </Center>
                     </Flex>
                 </CardBody>
