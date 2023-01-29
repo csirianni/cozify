586b0df79d57c35cf36cf06d317284b3c7ed3f0a
Cedric Sirianni <cds1@sirianni.org>
2023-01-29 10:15:02 -0500

    add custom font

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom';
+import '@fontsource/inter/500.css'
+
+import theme from "./styles/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
