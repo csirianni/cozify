c455a8ca6f7c8dc91b92baa0d22fa08249076ccc
Daniel <daniel.mekuriaw16@gmail.com>
2023-01-29 11:36:03 -0500

    Audios uploaded, external links working

--- frontend/src/pages/Songs.tsx
+++ frontend/src/pages/Songs.tsx
@@ -8,11 +8,11 @@ function Songs() {
                 <VStack width="80%">
                     <Text fontSize="5xl" color="white" marginBottom="10">Choose a cozy song.</Text>
                     <VStack width="100%">
-                        <Song title="Cozy #1" duration="0:49" url="http://streaming.tdiradio.com:8000/house.mp3" />
-                        <Song title="Cozy #2" duration="0:48" url="http://streaming.tdiradio.com:8000/house.mp3" />
-                        <Song title="Cozy #3" duration="0:37" url="http://streaming.tdiradio.com:8000/house.mp3" />
-                        <Song title="Cozy #4" duration="0:43" url="http://streaming.tdiradio.com:8000/house.mp3" />
-                        <Song title="Cozy #5" duration="0:49" url="http://streaming.tdiradio.com:8000/house.mp3" />
+                        <Song title="Cozy #1" duration="0:49" url="https://audio.jukehost.co.uk/GmWSEIK82RiofZ9f6FuaajgJl47ZQ9Z6" />
+                        <Song title="Cozy #2" duration="0:48" url="https://audio.jukehost.co.uk/I6pGuT884650qRmjvvygs3QtVhr6kaFl" />
+                        <Song title="Cozy #3" duration="0:37" url="https://audio.jukehost.co.uk/eQeIVdsVPLdqPP0kRNNkXUmh0icXeAnv" />
+                        <Song title="Cozy #4" duration="0:43" url="https://audio.jukehost.co.uk/7mhgd5lWbeZKvqKb02YTb9ej8PE8KvhW" />
+                        <Song title="Cozy #5" duration="0:49" url="https://audio.jukehost.co.uk/aQvQaE5PVd9puf56QliwWDyyXxVNFJ0b" />
                     </VStack>
                 </VStack>
             </Flex>
